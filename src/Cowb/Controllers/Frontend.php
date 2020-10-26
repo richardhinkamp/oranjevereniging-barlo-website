1890513706fdf595006abeebcea1d61bfb3a54e0
Richard Hinkamp <richard@hinkamp.nl>
2020-10-26 22:07:22 +0100

    Prototype sponsor page

--- src/Cowb/Controllers/Frontend.php
+++ src/Cowb/Controllers/Frontend.php
@@ -3,8 +3,14 @@
 namespace Cowb\Controllers;
 
 use Silex;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Mollie\Api\MollieApiClient;
+use Mollie\Api\Types\PaymentStatus;
+
+function getMollie() {
+    $mollie = new MollieApiClient();
+    $mollie->setApiKey(require(__DIR__ . '/../../../config/mollie.php'));
+    return $mollie;
+}
 
 /**
  * Standard Frontend actions
@@ -29,5 +35,121 @@ class Frontend
         } else {
             return $app['render']->render('emailconfirmfailed.twig');
         }
+    }
+    
+    public static function sponsor(Silex\Application $app)
+    {
+        if($app['request']->server->get('REQUEST_METHOD') == 'POST') {
+            $name = $app['request']->request->get('name');
+            $email = $app['request']->request->get('email');
+            $bouwstenen = intval($app['request']->request->get('bouwstenen'));
+            $gift = intval($app['request']->request->get('gift'));
+            $amount = ($bouwstenen * 50) + $gift;
+            if(trim($name) != '' && trim($email) != '' && filter_var($email, FILTER_VALIDATE_EMAIL) && $bouwstenen >=0 && $gift >= 0 && $amount > 0) {
+                if(intval($app['request']->request->get('confirm')) == 1) {
+                    $added = $app['request']->request->get('added');
+                    $item = new \Bolt\Content($app, 'sponsoren', array());
+                    $item->values['name'] = $name;
+                    $item->values['email'] = $email;
+                    $item->values['bouwstenen'] = $bouwstenen;
+                    $item->values['gift'] = $gift;
+                    $item->values['amount'] = $amount;
+                    $item->values['added'] = $added;
+                    $item->values['confirmed'] = date("Y-m-d H:i:s");
+                    $id = $app['storage']->saveContent($item);
+                    $mollie = getMollie();
+                    $payment = $mollie->payments->create([
+                        "amount" => [
+                            "currency" => "EUR",
+                            "value" => (string)$amount . '.00',
+                        ],
+                        "description" => "Bijdrage Groot Deunk",
+                        "redirectUrl" => "http://oranjeverenigingbarlo.local/pay/return/" . $id . '/' . md5(join('#', array($name, $email, $bouwstenen, $gift, $added))),
+                        // "webhookUrl"  => "http://oranjeverenigingbarlo.local/pay/webhook/",
+                    ]);
+                    $item->values['payment_id'] = $payment->id;
+                    $item->values['payment_status'] = $payment->status;
+                    $app['storage']->saveContent($item);
+                    $payment->redirectUrl .= '/' . $id . '/' . md5($id . '#' . $payment->id);
+                    header("Location: " . $payment->getCheckoutUrl(), true, 303);
+                    die();
+                    $app['storage']->saveContent($item);
+                    return $app['render']->render('sponsor-bedankt.twig');
+                } else {
+                    $app['twig']->addGlobal('name', $name);
+                    $app['twig']->addGlobal('email', $email);
+                    $app['twig']->addGlobal('bouwstenen', $bouwstenen);
+                    $app['twig']->addGlobal('gift', $gift);
+                    $app['twig']->addGlobal('amount', $amount);
+                    $app['twig']->addGlobal('added', date("Y-m-d H:i:s"));
+                    return $app['render']->render('sponsor-overzicht.twig');
+                }
+            } else {
+                $app['twig']->addGlobal('error', true);
+                $app['twig']->addGlobal('name', $name);
+                $app['twig']->addGlobal('email', $email);
+                $app['twig']->addGlobal('bouwstenen', $bouwstenen > 0 ? $bouwstenen : '');
+                $app['twig']->addGlobal('gift', $gift > 0 ? $gift : '');
+                return $app['render']->render('sponsor.twig');
+            }
+        } else {
+            $app['twig']->addGlobal('error', false);
+            $app['twig']->addGlobal('name', '');
+            $app['twig']->addGlobal('email', '');
+            $app['twig']->addGlobal('bouwstenen', '');
+            $app['twig']->addGlobal('gift', '');
+            return $app['render']->render('sponsor.twig');
+        }
+    }
+    
+    public static function payReturnId(Silex\Application $app, $id, $hash)
+    {
+        $record = $app['db']->fetchAssoc("SELECT * FROM bolt_sponsoren WHERE id = ?", array(intval($id)));
+        if($record) {
+            $recordHash = md5(join('#', array($record['name'], $record['email'], $record['bouwstenen'], $record['gift'], $record['added'])));
+            if($hash == $recordHash) {
+                $item = new \Bolt\Content($app, 'sponsoren', $record);
+                $mollie = getMollie();
+                $payment = $mollie->payments->get($record['payment_id']);
+                if($payment) {
+                    $item->values['payment_status'] = $payment->status;
+                    $status = $payment->status;
+                    $app['storage']->saveContent($item);
+                    if($status == PaymentStatus::STATUS_PAID) {
+                        return $app['render']->render('sponsor-bedankt.twig');
+                    } else if($status == PaymentStatus::STATUS_FAILED || $status == PaymentStatus::STATUS_CANCELED) {
+                        return $app['render']->render('sponsor-failed.twig');
+                    } else {
+                        return $app['render']->render('sponsor-pending.twig');
+                    }
+                }
+            }
+        }
+        header("Location: /sponsor", true, 301);
+        die();
+    }
+    
+    public static function payReturn(Silex\Application $app)
+    {
+        return $app['render']->render('sponsor-pending.twig');
+    }
+    
+    public static function pay(Silex\Application $app)
+    {
+        $mollie = new \Mollie\Api\MollieApiClient();
+        $mollie->setApiKey("test_BE5bgAm8ghVw9qxW4xVwuDsNK6K88z");
+        $payment = $mollie->payments->create([
+            "amount" => [
+                "currency" => "EUR",
+                "value" => "10.00"
+            ],
+            "description" => "My first API payment",
+            "redirectUrl" => "http://oranjeverenigingbarlo.local/pay/return",
+            // "webhookUrl"  => "http://oranjeverenigingbarlo.local/pay/webhook/",
+        ]);
+        header("Location: " . $payment->getCheckoutUrl(), true, 303);
+        die();
+        die($payment->getCheckoutUrl());
+        return $app['render']->render('sponsor.twig');
     }
 }
